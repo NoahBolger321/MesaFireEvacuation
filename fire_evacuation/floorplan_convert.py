f144f780cf71682fea0a5e923608e5fd65d782c0
Ameneh <ameneh.boroomand@mappedin.ca>
2022-02-04 13:18:21 -0500

    added windows to Exit

--- fire_evacuation/floorplan_convert.py
+++ fire_evacuation/floorplan_convert.py
@@ -29,6 +29,14 @@ def add_border_img(img):
     
     return border
 
+def down_sample_venue(img):
+    height, width = img.shape[:2]
+    while height > 200 and width > 200:
+        img = cv2.pyrDown(img)
+        height, width = img.shape[:2]
+        print(height, width)
+    return(img)    
+
 
 def color_threshold(img, thresholds=[]):
     result = img.copy()
@@ -93,25 +101,24 @@ def convert(GAN_file):
     combined_img = add_obstacles_to_GAN(GAN_file)
 
     combined_img = add_border_img(combined_img)
-
-    height, width = combined_img.shape[:2]
-    while height > 200 and width > 200:
-        combined_img = cv2.pyrDown(combined_img)
-        height, width = combined_img.shape[:2]
-
+    combined_img = down_sample_venue(combined_img)
 
     txt_floorplan = np.zeros(combined_img.shape[:2], 'U1')
     txt_floorplan.fill('E')
 
     walls = get_wall_image_layer(combined_img)
+    windows = get_window_image_layer(combined_img)
     doors = get_door_image_layer(combined_img)
     obstacles = get_obstacle_image_layer(combined_img)
 
     final_mask = get_final_mask(combined_img, GAN_file)
+    cv2.imwrite(f'{ROOT_DIR}/input/final_mask.png', walls)
+    
 
     txt_floorplan[np.where(final_mask == 0)[:2]] = "_"
     txt_floorplan[np.where(walls != (255, 255, 255))[:2]] = "W"
     txt_floorplan[np.where(doors != (255, 255, 255))[:2]] = "D"
     txt_floorplan[np.where(obstacles != (255, 255, 255))[:2]] = "F"
+    txt_floorplan[np.where(windows != (255, 255, 255))[:2]] = "E"
 
     np.savetxt(f"{ROOT_DIR}/fire_evacuation/floorplans/test_floorplan.txt", txt_floorplan, fmt="%s")
